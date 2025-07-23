6e3fb3917a8cae25080c57b3bf2623c9bc8a3a86
Dylan Randall <dylan.randall@abt.com>
2025-07-23 17:28:28 -0500

    started adding and styling hero and specials sections in Main.js. Started adding some global styles to App.css

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,39 @@
+import heroImg from './images/heroChef.png';
+import './Main.css'
 
 const Main = () => {
     return(
         <main>
+            <section class='hero'>
+                <div class='column1'>
+                    <div class='text-section'>
+                        <h1>Little Lemon</h1>
+                        <h2>Chicago</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    </div>
+                    <button>Reserve a Table</button>
+                </div>
+                <div>
+                    <img src={heroImg}></img>
+                </div>
+            </section>
+            <section class='Specials'>
+                <div class='first-row'>
+                    <h1>Specials</h1>
+                    <button>Online Menu</button>
+                </div>
+                <div class='second-row'>
+                    <div class='card'>
+                        <img></img>
+                        <div class='card content'>
+                            <p>Greek Salad</p>
+                            <p>$12.99</p>
+                            <p>The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. </p>
+                            <p>Order a delivery</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
 
         </main>
     )
