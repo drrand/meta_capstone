5c73da474125c8055cca2c40ae4030d0a6726d92
Dylan Randall <dylan.randall@abt.com>
2025-08-04 16:08:47 -0500

    changed to simpler form control, using a single useState for each field

--- src/BookingForm.js
+++ src/BookingForm.js
@@ -2,22 +2,50 @@ import { useEffect, useState } from "react";
 
 const BookingForm = () => {
 
-    const [formState, setFormState] = useState({
-        resDate: '',
-        resTime: '',
-        guests: '',
-        occasion: ''
-    });
+    // const [formState, setFormState] = useState({
+    //     resDate: '',
+    //     resTime: '',
+    //     guests: '',
+    //     occasion: ''
+    // });
 
-    function handleChange(event) {
-        const { name, value } = event.target;
-        // Handle form changes
-        setFormState((prevState) => (
-            {...prevState,[name]: value  } 
-        ))
+    const [resDate, setResDate] = useState('');
+    function handleResDateChange(event) {
+        const {value} = event.target;
+        setResDate(value);
     }
 
-    useEffect(() => console.log(formState), [formState])
+    const [resTime, setResTime] = useState();
+    function handleResTimeChange(event) {
+        const {value} = event.target;
+        setResTime(value);
+    }
+
+    const [guestNum, setGuestNum] = useState();
+    function handleGuestNumChange(event) {
+        const {value} = event.target;
+        setGuestNum(value);
+    }
+
+    const [occasion, setOccasion] = useState();
+    function handleOccasionChange(event) {
+        const {value} = event.target;
+        setOccasion(value);
+    }
+
+    // function handleChange(event) {
+    //     const { name, value } = event.target;
+    //     // Handle form changes
+    //     setFormState((prevState) => (
+    //         {...prevState,[name]: value  } 
+    //     ))
+    // }
+
+    // useEffect(() => console.log(formState), [formState])
+    useEffect(()=>console.log(resDate),[resDate])
+    useEffect(()=>console.log(resTime),[resTime])
+    useEffect(()=>console.log(guestNum),[guestNum])
+    useEffect(()=>console.log(occasion),[occasion])
 
     return (
         <form style={{
@@ -27,17 +55,19 @@ const BookingForm = () => {
         }}>
             <label htmlFor="resDate">Choose date</label>
             <input
-                name="resDate" 
-                type="date" 
+                name="resDate"
+                type="date"
                 id="resDate"
-                onChange={handleChange} 
-                value={formState.resDate}
-                />
+                onChange={(event) => handleResDateChange(event)}
+                value={resDate}
+            />
             <label htmlFor="resTime">Choose time</label>
-            <select 
-                name="resTime" 
+            <select
+                name="resTime"
                 id="resTime"
-                onChange={handleChange}>
+                onChange={(event) =>handleResTimeChange(event)}
+                value={resTime}
+                >
                 <option>17:00</option>
                 <option>18:00</option>
                 <option>19:00</option>
@@ -46,21 +76,22 @@ const BookingForm = () => {
                 <option>22:00</option>
             </select>
             <label htmlFor="guests">Number of guests</label>
-            <input 
-                name="guests" 
-                type="number" 
-                placeholder="1" 
-                min="1" 
-                max="10" 
+            <input
+                name="guests"
+                type="number"
+                placeholder="1"
+                min="1"
+                max="10"
                 id="guests"
-                onChange={handleChange} 
-                />
+                onChange={handleGuestNumChange}
+                value={guestNum}
+            />
             <label htmlFor="occasion">Occasion</label>
-            <select 
-                name="occasion" 
+            <select
+                name="occasion"
                 id="occasion"
-                onChange={handleChange}
-                >
+                onChange={handleOccasionChange}
+            >
                 <option>Birthday</option>
                 <option>Anniversary</option>
             </select>
